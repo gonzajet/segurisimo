b43f37bb381554b48f9c74bd2784d385c025de39
Gonzalo <gonzajet1@gmail.com>
2018-07-02 21:59:00 -0300

    agregando 2 botones a la vista

--- app/src/main/java/fragments/DetalleSiniestroFragment.java
+++ app/src/main/java/fragments/DetalleSiniestroFragment.java
@@ -6,7 +6,10 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -82,9 +85,11 @@ public class DetalleSiniestroFragment extends Fragment implements OnMapReadyCall
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-
     }
+
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
